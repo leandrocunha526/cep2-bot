6108ea25f4cece16e0b0fedd879b2bfa1baa0f62
leandrocunha526 <leandrocunha016@gmail.com>
2021-12-27 20:42:39 -0300

    first commit

---
+++ Procfile
@@ -0,0 +1,3 @@
+web: python3 bot_cep.py
+
+worker: python3 bot_cep.py  
---
+++ bot_cep.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+import requests
+import json
+import telepot
+from telepot.loop import MessageLoop
+import time
+
+
+def handle(msg):
+    typemsg, typechat, chat_id = telepot.glance(msg)
+    command = msg['text'].split(' ')
+    if msg['text'] == '/start':
+        welcome = 'Seja bem vindo ao CEP2Bot'
+        bot.sendMessage(chat_id, welcome)
+    if msg['text'] == '/cep':
+        msg = 'Digite o cep para buscar'
+        bot.sendMessage(chat_id, msg)
+    if command[0] == '/cep':
+        cep = command[1]
+        api = "https://viacep.com.br/ws/"+cep+"/json/unicode/"
+        r = requests.get(api)
+        results = json.loads(r.content)
+        print(results)
+        txt = 'Consulta de CEP e IBGE no Telegram:\nCEP: ' + results[
+            'cep'] + '\nLogradouro: ' + results['logradouro'] + '\nComplemento: ' + results[
+                  'complemento'] + '\nBairro: ' + results['bairro'] + '\nLocalidade: ' + results[
+                  'localidade'] + '\nUF: ' + results['uf'] + '\nIBGE: ' + results['ibge'] + '\nDDD: ' + results['ddd']
+        bot.sendMessage(chat_id, txt)  # send result
+
+
+TOKEN = ''  # your token
+
+bot = telepot.Bot(TOKEN)
+ot = telepot.Bot(TOKEN)
+MessageLoop(bot, handle).run_as_thread()
+print('Please wait')
+print('Modo de uso para consultas: /cep 12345-123')
+
+while 1:
+    time.sleep(10)
